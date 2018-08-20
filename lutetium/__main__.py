36f4f181c475ceaf1971c0d78ffac432ae5a292a
dequis <dx@dxzone.com.ar>
2018-08-20 04:29:26 -0300

    main: add --meter-source and --pvsim-source options

--- lutetium/__main__.py
+++ lutetium/__main__.py
@@ -4,10 +4,10 @@ import aioamqp
 import logging
 import functools
 
-from . import sources
 from .meter import Meter
 from .pvsim import PVSim
 from .writer import Writer
+from .sources import SOURCE_REGISTRY
 
 TASKS = {
     'meter': [Meter],
@@ -17,22 +17,32 @@ TASKS = {
 
 TASKS['all'] = [x[0] for x in TASKS.values()]
 
+source_choice = click.Choice(SOURCE_REGISTRY.keys())
+
 @click.command()
 @click.option('-v', '--verbose', count=True)
 @click.option('-f', '--filename', default='out.jsonl')
+@click.option('--meter-source',
+    type=source_choice,
+    default='random'
+)
+@click.option('--pvsim-source',
+    type=source_choice,
+    default='noisy_absurd_polynomial'
+)
 @click.argument('tasks',
     nargs=-1,
     required=True,
     type=click.Choice(TASKS.keys())
 )
-def main(verbose, filename, tasks):
+def main(verbose, filename, meter_source, pvsim_source, tasks):
     levels = [logging.WARNING, logging.INFO, logging.DEBUG]
 
     logging.basicConfig(level=levels[min(2, verbose)])
 
     obj = {}
-    obj['meter_source'] = sources.RandomMeterSource()
-    obj['pvsim_source'] = sources.NoisyAbsurdPolynomialFitSource()
+    obj['meter_source'] = SOURCE_REGISTRY[meter_source]()
+    obj['pvsim_source'] = SOURCE_REGISTRY[pvsim_source]()
     obj['filename'] = filename
 
     coros = []
