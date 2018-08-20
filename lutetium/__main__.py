a13bdc676a653e4570e6af4b2641a2b34f0a0642
dequis <dx@dxzone.com.ar>
2018-08-20 03:46:04 -0300

    main: rewrite, simplify and include writer command

--- lutetium/__main__.py
+++ lutetium/__main__.py
@@ -4,50 +4,46 @@ import aioamqp
 import logging
 import functools
 
+from . import sources
 from .meter import Meter
 from .pvsim import PVSim
-from . import sources
+from .writer import Writer
+
+TASKS = {
+    'meter': [Meter],
+    'pvsim': [PVSim],
+    'writer': [Writer],
+}
+
+TASKS['all'] = [x[0] for x in TASKS.values()]
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
 
-def run_forever(*coros):
+@click.command()
+@click.argument('tasks',
+    nargs=-1,
+    required=True,
+    type=click.Choice(TASKS.keys())
+)
+def main(tasks):
+    setup_logging()
+
+    obj = {}
+    obj['meter_source'] = sources.RandomMeterSource()
+    obj['pvsim_source'] = sources.NoisyAbsurdPolynomialFitSource()
+    obj['filename'] = 'out.jsonl'
+
+    coros = []
+
+    for name in tasks:
+        for cls in TASKS[name]:
+            instance = cls(**obj)
+            coros.append(instance.run())
+
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.gather(*coros))
     loop.run_forever()
-    return loop
-
-@click.group()
-@click.pass_context
-def cli(ctx):
-    setup_logging()
-    ctx.obj['meter_source'] = sources.RandomMeterSource()
-    ctx.obj['pvsim_source'] = sources.NoisyAbsurdPolynomialFitSource()
-
-def command(f):
-
-    @cli.command(name=f.__name__)
-    @click.pass_context
-    @functools.wraps(f)
-    def wrapper(ctx):
-        run_forever(*f(ctx))
-
-    wrapper.orig = f
-
-    return wrapper
-
-@command
-def meter(ctx):
-    return [Meter(**ctx.obj).run()]
-
-@command
-def pvsim(ctx):
-    return [PVSim(**ctx.obj).run()]
-
-@command
-def all(ctx):
-    return meter.orig(ctx) + pvsim.orig(ctx)
 
 if __name__ == '__main__':
-    cli(obj={})
-
+    main()
