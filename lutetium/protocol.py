bc28e7142c7d0096d64d140e19fa9cae2586b94f
dequis <dx@dxzone.com.ar>
2018-08-20 01:18:44 -0300

    protocol: rename sequence to seq

--- lutetium/protocol.py
+++ lutetium/protocol.py
@@ -9,13 +9,13 @@ class BaseMessage(Schema):
     Provides three fields:
 
      - type: a string with the name of the (sub)class
-     - sequence: an integer that is optionally checked against the previous
-       received value (see validate_sequence)
+     - seq: an integer that is optionally checked against the previous
+       received value (see validate_seq)
      - timestamp: optional time, for events. Rendered as an ISO8601 string.
     """
 
     type = fields.Method('get_type', required=True)
-    sequence = fields.Integer(required=True)
+    seq = fields.Integer(required=True)
     timestamp = fields.DateTime()
 
     def get_type(self, obj=None):
@@ -33,18 +33,18 @@ class BaseMessage(Schema):
         if value != self.get_type():
             raise ValidationError('Invalid object type')
 
-    @validates('sequence')
-    def validate_sequence(self, value):
+    @validates('seq')
+    def validate_seq(self, value):
         """Optional validator of sequence IDs
 
         Disables itself if no previous sequence ID is provided in the context,
         otherwise ensures it is the next one. Raises warnings only.
         """
 
-        if self.context.get('sequence', 0) == 0:
+        if self.context.get('seq', 0) == 0:
             return
 
-        expected = self.context['sequence'] + 1
+        expected = self.context['seq'] + 1
         if expected != value:
             logger.warning('%s: Expected sequence ID %s, got %s',
                 self.get_type(), expected, value)
