db79016afcf5088489e25332c01502aac5babb23
dequis <dx@dxzone.com.ar>
2018-08-20 03:24:07 -0300

    Add writer (only newline separated json for now)

---
+++ lutetium/writer.py
@@ -0,0 +1,27 @@
+import logging
+
+from . import common, protocol
+
+logger = logging.getLogger('lutetium.writer')
+
+class Writer(common.LutetiumCommon):
+    consume_queue = 'lutetium_pvsim'
+
+    def __init__(self, filename, **kwargs):
+        super().__init__(**kwargs)
+        self.file = open(filename, 'a')
+        self.seq = 0
+
+    async def on_event(self, channel, body, envelope, properties):
+        pv_message = protocol.PVMessage().loads(body)
+
+        self.file.write(body.decode() + '\n')
+        self.file.flush()
+
+        await channel.basic_client_ack(envelope.delivery_tag)
+
+    async def run(self):
+        await self.connect()
+
+        while True:
+            await self.consume(self.on_event)
